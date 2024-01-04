b671a973cbb2953e6bf6cdd1047e46c092bddc55
alexgcoleman <107187057+alexgcoleman@users.noreply.github.com>
2024-01-04 18:07:51 +1100

    add scrollable DataTable pane

--- src/pique/cli.py
+++ src/pique/cli.py
@@ -1,11 +1,29 @@
 import argparse
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Optional, Sequence
+from typing import Optional, Sequence, Type
 
+import polars as pl
+from polars import datatypes
+from rich.text import Text
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, Static
+from textual.widgets import DataTable, Footer, Static
 
 from . import engine
+
+
+@dataclass
+class DTypeFormat:
+    colour: str = "white"
+    justify: str = "center"
+    overflow: str | None = None
+
+
+DTYPE_STYLE: dict[Type, DTypeFormat] = {
+    datatypes.String: DTypeFormat(colour="green", justify="left", overflow="ellipsis"),
+    datatypes.Float64: DTypeFormat(colour="magenta", justify="right"),
+    datatypes.Int64: DTypeFormat(colour="blue", justify="center"),
+}
 
 
 class DataPane(Static):
@@ -19,8 +37,29 @@ class DataPane(Static):
 
     def compose(self) -> ComposeResult:
         """Create child widgets of a stopwatch."""
-        data = engine.reader(self.filename)
-        yield Static(f"{data}")
+        yield DataTable()
+
+    def on_mount(self) -> None:
+        df = engine.reader(self.filename).head(80)
+
+        cols = df.columns
+
+        nulls = {col: df.select(pl.col(col).is_null()) for col in cols}
+        styling = {col: DTYPE_STYLE.get(df[col].dtype, Text) for col in cols}
+
+        rows = [
+            [
+                format_cell(
+                    repr(df[col][row]), fmt=styling[col], is_na=nulls[col].row(row)[0]
+                )
+                for col in cols
+            ]
+            for row in range(len(df))
+        ]
+
+        table = self.query_one(DataTable)
+        table.add_columns(*cols)
+        table.add_rows(rows)
 
 
 class Pique(App):
@@ -35,7 +74,6 @@ class Pique(App):
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
-        yield Header()
         yield DataPane(filename=self.filename)
         yield Footer()
 
@@ -67,3 +105,8 @@ def parse_args(argv: Optional[Sequence[str]] = None) -> argparse.Namespace:
 def cli() -> None:
     app = Pique()
     app.run()
+
+
+def format_cell(cell_repr: str, fmt: DTypeFormat, is_na: bool = False) -> Text:
+    colour = fmt.colour if not is_na else "gray"
+    return Text(cell_repr, style=colour, overflow=fmt.overflow, justify=fmt.justify)
