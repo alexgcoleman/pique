4ab6e72f0ceebad26e82ce5f5380aa92b42b7db6
alexgcoleman <107187057+alexgcoleman@users.noreply.github.com>
2024-01-04 17:06:28 +1100

    initial textual app

--- src/pique/cli.py
+++ src/pique/cli.py
@@ -2,9 +2,46 @@ import argparse
 from pathlib import Path
 from typing import Optional, Sequence
 
-from rich import print
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header, Static
 
 from . import engine
+
+
+class DataPane(Static):
+    """Pane for displaying tabular data"""
+
+    filename: Path
+
+    def __init__(self, filename: Path, *args, **kwargs) -> None:
+        self.filename = filename
+        super().__init__(*args, **kwargs)
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets of a stopwatch."""
+        data = engine.reader(self.filename)
+        yield Static(f"{data}")
+
+
+class Pique(App):
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    filename: Path
+    dark: bool
+
+    def __init__(self, *args, **kwargs) -> None:
+        parsed_args = parse_args()
+        self.filename = parsed_args.filename
+        super().__init__(*args, **kwargs)
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield DataPane(filename=self.filename)
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
 
 
 def valid_filepath(s: str) -> Path:
@@ -19,13 +56,14 @@ def valid_filepath(s: str) -> Path:
     return path
 
 
-def cli(argv: Optional[Sequence[str]] = None) -> int:
+def parse_args(argv: Optional[Sequence[str]] = None) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
     parser.add_argument("filename", type=valid_filepath, help="path to file")
 
-    args = parser.parse_args(argv)
+    return parser.parse_args(argv)
 
-    print(engine.reader(args.filename))
 
-    return 0
+def cli() -> None:
+    app = Pique()
+    app.run()
